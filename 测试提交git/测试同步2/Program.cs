bc47e05a98d97d3b3ae53caf5021786ebc8c8b7c
442601340 <442601340@qq.com>
2021-12-27 13:18:24 +0800

    新增测试2.0

---
+++ 测试提交git/测试同步2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 测试同步2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("a = {0}",a);
+            Console.ReadLine();
+        }
+    }
+}
